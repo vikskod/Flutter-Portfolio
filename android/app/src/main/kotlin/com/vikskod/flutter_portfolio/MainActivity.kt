b6fa7c770f3cc88a70a3c8e0316726b9b18c9bbc
vikash <vparajuli819@gmail.com>
2021-03-05 23:43:18 +1100

    First commit

---
+++ android/app/src/main/kotlin/com/vikskod/flutter_portfolio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vikskod.flutter_portfolio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/util/strings.dart
@@ -0,0 +1,4 @@
+class Strings {
+  static const String myName = "Vikash Parajuli";
+  static const String welcomeMessage = "Welcome To Vikash Portfolio";
+}
